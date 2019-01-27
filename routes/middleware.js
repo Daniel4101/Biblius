5ee9896dd049eb5496f19061b71f3f6b346e95ee
Paweł Kolasiński <pawel.kolasinski@optize.pl>
2019-01-27 09:46:08 +0100

    Update config

--- routes/middleware.js
+++ routes/middleware.js
@@ -19,10 +19,10 @@ var _ = require('lodash');
 */
 exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
-		{ label: 'Home', key: 'home', href: '/' },
-		{ label: 'Blog', key: 'blog', href: '/blog' },
-		{ label: 'Gallery', key: 'gallery', href: '/gallery' },
-		{ label: 'Contact', key: 'contact', href: '/contact' },
+		{ label: 'Książki', key: 'books', href: '/books' },
+		{ label: 'Filmy i grafika', key: 'gallery', href: '/gallery' },
+		{ label: 'Aktualności', key: 'blog', href: '/blog' },
+		{ label: 'O bibliotece', key: 'contact', href: '/contact' },
 	];
 	res.locals.user = req.user;
 	next();
